b973755ce677792474480879a35e61b5c5c940e2
rorlich <rorlich@gmail.com>
2022-08-17 15:21:06 +0300

    commentout ts issues

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,43 +1,43 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { getPostListings } from "~/models/post.server";
-import { useOptionalAdminUser } from "~/utils";
+// import { Link, useLoaderData } from "@remix-run/react";
+// import type { LoaderFunction } from "@remix-run/node";
+// import { json } from "@remix-run/node";
+// import { getPostListings } from "~/models/post.server";
+// import { useOptionalAdminUser } from "~/utils";
 
-type LoaderData = {
-  posts: Awaited<ReturnType<typeof getPostListings>>;
-};
+// type LoaderData = {
+//   posts: Awaited<ReturnType<typeof getPostListings>>;
+// };
 
-export const loader: LoaderFunction = async () => {
-  const posts = await getPostListings();
-  return json<LoaderData>({ posts });
-};
+// export const loader: LoaderFunction = async () => {
+//   const posts = await getPostListings();
+//   return json<LoaderData>({ posts });
+// };
 
-export default function PostsRoute() {
-  const { posts } = useLoaderData() as LoaderData;
-  const adminUser = useOptionalAdminUser();
+// export default function PostsRoute() {
+//   const { posts } = useLoaderData() as LoaderData;
+//   const adminUser = useOptionalAdminUser();
 
-  return (
-    <main>
-      <h1>Posts</h1>
-      {adminUser ? (
-        <Link to="admin" className="text-red-600 underline">
-          Admin
-        </Link>
-      ) : null}
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link
-              to={post.slug}
-              prefetch="intent"
-              className="text-blue-600 underline"
-            >
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
-}
+//   return (
+//     <main>
+//       <h1>Posts</h1>
+//       {adminUser ? (
+//         <Link to="admin" className="text-red-600 underline">
+//           Admin
+//         </Link>
+//       ) : null}
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.slug}>
+//             <Link
+//               to={post.slug}
+//               prefetch="intent"
+//               className="text-blue-600 underline"
+//             >
+//               {post.title}
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </main>
+//   );
+// }
