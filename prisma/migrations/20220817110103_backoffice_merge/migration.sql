9176c12a5d9923f8fa4256afa47a9b92ee4da970
rorlich <rorlich@gmail.com>
2022-08-17 14:16:27 +0300

    prisma migrate

---
+++ prisma/migrations/20220817110103_backoffice_merge/migration.sql
@@ -0,0 +1,101 @@
+-- CreateTable
+CREATE TABLE "CarmelaProduct" (
+    "id" VARCHAR(128) NOT NULL,
+    "name" VARCHAR(128) NOT NULL,
+    "tags_names" TEXT[],
+    "slug" TEXT NOT NULL,
+    "onsale" BOOLEAN NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "selected" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "weight" DOUBLE PRECISION,
+    "weight_type" TEXT,
+    "imageBig" TEXT,
+    "salePrice" TEXT,
+    "description" TEXT NOT NULL,
+    "instock" BOOLEAN NOT NULL,
+
+    CONSTRAINT "CarmelaProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" VARCHAR(200) NOT NULL,
+    "status" VARCHAR(64) NOT NULL,
+    "type" VARCHAR(64) NOT NULL,
+    "type_extra_info" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "from" VARCHAR(64) NOT NULL,
+    "contactId" TEXT NOT NULL,
+    "conversation" JSONB,
+    "pricing" JSONB,
+    "sessionId" TEXT NOT NULL,
+    "state" TEXT,
+    "contextMessageId" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" VARCHAR(200) NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end" TIMESTAMP(3),
+    "customer_id" VARCHAR(64) NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "mobile" VARCHAR(16) NOT NULL,
+    "firstName" VARCHAR(64) NOT NULL,
+    "lastName" VARCHAR(64),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Password" (
+    "hash" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CarmelaProduct_id_key" ON "CarmelaProduct"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_id_key" ON "Message"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_contextMessageId_key" ON "Message"("contextMessageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_id_key" ON "Session"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mobile_key" ON "User"("mobile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "User"("mobile") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_contextMessageId_fkey" FOREIGN KEY ("contextMessageId") REFERENCES "Message"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
