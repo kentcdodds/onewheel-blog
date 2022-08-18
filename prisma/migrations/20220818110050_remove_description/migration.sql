df5d86f6b83ada7866d5103482e2f5ddf8136eb5
rorlich <rorlich@gmail.com>
2022-08-18 14:01:47 +0300

    migration

---
+++ prisma/migrations/20220818110050_remove_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `CarmelaProduct` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CarmelaProduct" DROP COLUMN "description";
