ae7bb58f4790bba81566189be65d829bfccd572a
rorlich <rorlich@gmail.com>
2022-08-22 16:45:45 +0300

    profile slug

--- app/routes/admin/profile/$slug.tsx
+++ app/routes/admin/profile/$slug.tsx
@@ -1,6 +1,6 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useCatch, useLoaderData, useParams } from "@remix-run/react";
+import { Form, useCatch, useLoaderData, useParams } from "@remix-run/react";
 import { getProfile } from "~/models/profile.server";
 import invariant from "tiny-invariant";
 import { getProduct } from "~/models/product.server";
@@ -9,14 +9,15 @@ type LoaderData = {
     customer: any;
     orders: any;
     profile: any;
+    recommendation: any;
 };
 
 type ProductData = {
-    product_id: string,
-    name: string,
-    count: number,
-    order_percentage: number
-    image: string
+    product_id: string;
+    name: string;
+    count: number;
+    order_percentage: number;
+    info: any;
 }
 
 export const loader: LoaderFunction = async ({ params }) => {
@@ -28,25 +29,45 @@ export const loader: LoaderFunction = async ({ params }) => {
         throw new Response("Not Found", { status: 404 });
     }
 
-    var startTime = performance.now()
-    for (let i = 0; i < profile.profile.length; i++) {
-        const product = await getProduct(profile.profile[i].product_id.toString())
-        profile.profile[i].image = product?.image ?? "";
-    }
-    var endTime = performance.now();
-    console.log(`products fetch ${endTime - startTime} milliseconds`);
-
-    return json<LoaderData>({ customer: profile.customer, orders: profile.orders, profile: profile.profile });
+    // var startTime = performance.now()
+    // for (let i = 0; i < profile.profile.length; i++) {
+    //     const product = await getProduct(profile.profile[i].product_id.toString())
+    //     profile.profile[i].image = product?.image ?? "";
+    // }
+    // var endTime = performance.now();
+    // console.log(`products fetch ${endTime - startTime} milliseconds`);
+    
+    return json<LoaderData>({ customer: profile.customer, orders: profile.orders, profile: profile.profile, recommendation: profile.profile.slice(0, 15) });
 };
 
-export default function PostRoute() {
-    const { customer, orders, profile } = useLoaderData() as LoaderData;
+export default function ProfileRoute() {
+    const { customer, orders, profile, recommendation } = useLoaderData() as LoaderData;
     return (
         <main className="mx-auto max-w-4xl">
-            <h1 className="my-6 border-b-2 text-center text-3xl">{customer.email}</h1>
-            <h2 className="my-6 text-center text-3xl">{customer.first_name} {customer.last_name}</h2>
-            <h2 className="my-6 text-center text-3xl">{customer.billing?.phone}</h2>
-            <h2 className="my-6 text-center text-3xl">{orders.length} total orders</h2>
+            <h1 className="my-6 text-center text-3xl">{customer.email}</h1>
+            <h2 className="my-6 text-center text-2xl border-b-2">{customer.first_name} {customer.last_name} {customer.billing?.phone}</h2>
+
+            <h2 className="my-6 text-2xl">Next cart recommendation:</h2>
+            <div className="grid grid-cols-4 gap-4 mb-10 mt-4">
+            {recommendation.map((product: ProductData) => (
+                <div key={product.product_id}>
+                    <img className="object-cover h-20" src={product.info.image} alt={product.info.image}></img>
+                    <p>{product.name}</p>
+                </div>
+            ))}
+            </div>
+
+            <p>Next Order suggestion: 01/01/2020</p>
+            
+            <div className="flex justify-center">
+                <Form method="post" className="space-y-6">
+                    <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        Approve Recommendation
+                    </button>
+                </Form>
+            </div>
+            <h2 className="my-6 text-2xl">Profile</h2>
+            <h2 className="my-6 text-l">Profile based on {orders.length} past orders</h2>
 
             {orders.length == 0 ? (
                 <h1>No orders for this customers.</h1>
@@ -68,7 +89,7 @@ export default function PostRoute() {
                                 <td>{product.name}</td>
                                 <td>{product.count}</td>
                                 <td>{product.order_percentage}</td>
-                                {<td><img className="object-cover h-20" src={product.image} alt={product.image}></img></td>}
+                                {<td><img className="object-cover h-20" src={product.info.image} alt={product.info.image}></img></td>}
                             </tr>
                         ))}
                     </tbody>
